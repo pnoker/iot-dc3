c5a7f5a8c4a58fd10541c3662d71e404b8709adb
pnoker <pnokers@gmail.com>
2019-04-13 15:13:41 +0800

    feat(gateway): add api gateway service with dev/prod config

---
+++ dc3-gateway/src/main/java/com/pnoker/GatewayApplication.java
@@ -0,0 +1,18 @@
+package com.pnoker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author: Pnoker
+ * @email : pnokers@gmail.com
+ */
+@EnableZuulProxy
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
