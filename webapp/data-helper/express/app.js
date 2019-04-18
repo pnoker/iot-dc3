c6d5683f20e51dd472ec3963ce6531ca6c253846
pnoker <pnokers@gmail.com>
2019-04-18 13:44:28 +0800

    fix(webapp): clean up express app.js routes and fix router path in data-helper

--- webapp/data-helper/express/app.js
+++ webapp/data-helper/express/app.js
@@ -6,10 +6,6 @@ let logger = require('morgan');
 
 let app = express();
 
-// view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
--- webapp/data-helper/src/router.js
+++ webapp/data-helper/src/router.js
@@ -4,7 +4,7 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
-    mode: 'history',
+    mode: 'hash',
     routes: [
         {
             path: '/',
