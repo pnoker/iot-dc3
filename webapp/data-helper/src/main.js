a999cf0e2ef6f26a80f196070a2893428a9e411c
pnoker <pnokers@gmail.com>
2019-04-18 09:48:11 +0800

    express

--- webapp/data-helper/src/main.js
+++ webapp/data-helper/src/main.js
@@ -3,10 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import ElementUI from 'element-ui';
+import locale from 'element-ui/lib/locale/lang/en'
+import 'element-ui/lib/theme-chalk/index.css';
+
 Vue.config.productionTip = false
 
+Vue.use(ElementUI, {locale});
+
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
