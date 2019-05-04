8c72705c21f497ad31f3900405a63621d17bde5c
pnoker <pnokers@gmail.com>
2019-05-04 23:31:41 +0800

    test(dbs): add rtmp mapper, model, and mybatis test with datasource configuration

---
+++ dc3-dbs/src/main/java/com/pnoker/dbs/model/Rtmp.java
@@ -0,0 +1,16 @@
+package com.pnoker.dbs.model;
+
+import lombok.Data;
+
+/**
+ * <p>Copyright(c) 2018. Pnoker All Rights Reserved.
+ * <p>Author     : Pnoker
+ * <p>Email      : pnokers@gmail.com
+ * <p>Description: Rtmp 信息实体类
+ */
+@Data
+public class Rtmp {
+    public static final String ID = "id";
+
+    private long id;
+}
