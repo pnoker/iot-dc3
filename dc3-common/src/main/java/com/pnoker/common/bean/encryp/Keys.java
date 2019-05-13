af2ea0b5873f5578f9a0d5d758bf993efcff851c
pnoker <pnokers@gmail.com>
2019-05-13 13:00:35 +0800

    feat(common): introduce aes and rsa encryption util classes, remove active tools, add keys bean

---
+++ dc3-common/src/main/java/com/pnoker/common/bean/encryp/Keys.java
@@ -0,0 +1,32 @@
+package com.pnoker.common.bean.encryp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * <p>Copyright(c) 2018. Pnoker All Rights Reserved.
+ * <p>Author     : Pnoker
+ * <p>Email      : pnokers@gmail.com
+ * <p>Description: AES & RSA 算法密钥实体类
+ */
+public class Keys {
+
+    /**
+     * RSA 密钥对
+     */
+    @Data
+    @AllArgsConstructor
+    public class Rsa {
+        private String publicKey;
+        private String privateKey;
+    }
+
+    /**
+     * Aes 密钥
+     */
+    @Data
+    @AllArgsConstructor
+    public class Aes {
+        private String privateKey;
+    }
+}
