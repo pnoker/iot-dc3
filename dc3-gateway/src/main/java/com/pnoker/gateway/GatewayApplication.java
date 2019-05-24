28872c6d96581fb9cdb6b9f46ae375d5fa94e11b
pnoker <pnokers@gmail.com>
2019-05-24 22:24:44 +0800

    提交spring cloud gateway配置

--- dc3-gateway/src/main/java/com/pnoker/gateway/GatewayApplication.java
+++ dc3-gateway/src/main/java/com/pnoker/gateway/GatewayApplication.java
@@ -2,13 +2,11 @@ package com.pnoker.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * @author: Pnoker
  * @email : pnokers@gmail.com
  */
-@EnableZuulProxy
 @SpringBootApplication
 public class GatewayApplication {
 
