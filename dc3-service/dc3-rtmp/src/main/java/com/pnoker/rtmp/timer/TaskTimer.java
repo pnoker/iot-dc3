69b37d4d7759200d04ec2869d289c3cd7f44bb95
pnoker <pnokers@gmail.com>
2019-05-26 23:58:43 +0800

    feat(rtmp): add task monitor thread, timer, and output handle for rtsp-to-rtmp relay

--- dc3-service/dc3-rtmp/src/main/java/com/pnoker/rtmp/timer/TaskTimer.java
+++ dc3-service/dc3-rtmp/src/main/java/com/pnoker/rtmp/timer/TaskTimer.java
@@ -13,28 +13,22 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.pnoker.rtmp;
+package com.pnoker.rtmp.timer;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 /**
  * <p>Copyright(c) 2018. Pnoker All Rights Reserved.
  * <p>Author     : Pnoker
  * <p>Email      : pnokers@gmail.com
- * <p>Description:
+ * <p>Description: 定时任务，用于定时启动 RTSP->RTMP 任务
  */
-@EnableAsync
-@EnableScheduling
-@EnableFeignClients
-@EnableEurekaClient
-@SpringBootApplication
-public class RtmpApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(RtmpApplication.class, args);
+@Slf4j
+@Component
+public class TaskTimer {
+    @Scheduled(fixedRate = 5000)
+    public void runRtmpTask() {
     }
 }
