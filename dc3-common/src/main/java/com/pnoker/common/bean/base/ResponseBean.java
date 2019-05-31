e66ec2e2fa821f20dae00fee757d88b7dc52d2f9
pnoker <pnokers@gmail.com>
2019-05-31 16:55:21 +0800

    日常提交

---
+++ dc3-common/src/main/java/com/pnoker/common/bean/base/ResponseBean.java
@@ -0,0 +1,19 @@
+package com.pnoker.common.bean.base;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * <p>Copyright(c) 2018. Pnoker All Rights Reserved.
+ * <p>Author     : Pnoker
+ * <p>Email      : pnokers@gmail.com
+ * <p>Description: 结果返回 实体类
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseBean {
+    private boolean result = false;
+    private Object message = "nothing";
+}
