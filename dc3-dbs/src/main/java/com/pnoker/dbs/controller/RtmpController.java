0abe995d27eed9b61529328d3b0811ac5a333401
pnoker <pnokers@gmail.com>
2019-06-03 01:07:35 +0800

    添加rtmp数据操作逻辑

--- dc3-dbs/src/main/java/com/pnoker/dbs/controller/RtmpController.java
+++ dc3-dbs/src/main/java/com/pnoker/dbs/controller/RtmpController.java
@@ -1,12 +1,16 @@
 package com.pnoker.dbs.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.pnoker.api.dbs.feign.RtmpFeignApi;
 import com.pnoker.common.base.BaseController;
 import com.pnoker.common.model.rtmp.Rtmp;
+import com.pnoker.dbs.service.RtmpService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>Copyright(c) 2018. Pnoker All Rights Reserved.
@@ -19,16 +23,34 @@ import org.springframework.web.bind.annotation.RestController;
 public class RtmpController extends BaseController implements RtmpFeignApi {
     @Autowired
     private Environment environment;
+    @Autowired
+    private RtmpService rtmpService;
 
     @Override
     public String api() {
-
         return environment.getProperty("server.port");
     }
 
     @Override
-    public Rtmp list() {
-        Rtmp rtmp = new Rtmp(1);
-        return rtmp;
+    public String add(String json) {
+        return null;
+    }
+
+    @Override
+    public String delete(String json) {
+        return null;
+    }
+
+    @Override
+    public String update(String json) {
+        return null;
+    }
+
+    @Override
+    public List<Rtmp> list() {
+        QueryWrapper<Rtmp> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("auto_start", true);
+        List<Rtmp> list = rtmpService.list(queryWrapper);
+        return list;
     }
 }
