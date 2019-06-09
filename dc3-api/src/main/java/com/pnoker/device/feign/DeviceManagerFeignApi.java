509798ffb732b4b3cca76ea582f51f73526f44d2
pnoker <pnokers@gmail.com>
2019-06-09 21:29:52 +0800

    feat(device): add dc3-client module with Feign API and hystrix fallback

---
+++ dc3-api/src/main/java/com/pnoker/device/feign/DeviceManagerFeignApi.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2018 Google LLC. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.pnoker.device.feign;
+
+import com.pnoker.common.model.rtmp.Rtmp;
+import com.pnoker.device.hystrix.DeviceManagerFeignApiHystrix;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * <p>Copyright(c) 2018. Pnoker All Rights Reserved.
+ * <p>Author     : Pnoker
+ * <p>Email      : pnokers@gmail.com
+ * <p>Description:
+ */
+@RequestMapping("/device/manager")
+@FeignClient(name = "DC3-DEVICE-MANAGER", fallbackFactory = DeviceManagerFeignApiHystrix.class)
+public interface DeviceManagerFeignApi {
+
+    @GetMapping(value = "/api")
+    String api();
+
+    @PostMapping(value = "/add")
+    String add(String json);
+
+    @DeleteMapping(value = "/delete")
+    String delete(String json);
+
+    @PutMapping(value = "/update")
+    String update(String json);
+
+    @GetMapping(value = "/list")
+    List<Rtmp> list();
+}
