6ff025ffafcf7a26518e96b49f823c4e5d42fbd7
pnoker <pnokers@gmail.com>
2019-07-09 17:35:29 +0800

    build: centralize spring-boot repackage config and align module dockerfiles

--- dc3/dependences/mariadb/iot-dc3.sql
+++ dc3/dependences/mariadb/iot-dc3.sql
@@ -1,7 +1,8 @@
 /*
-创建 MariaDB Database - iot-dc3
+SQLyog Ultimate v11.27 (32 bit)
+MySQL - 10.4.6-MariaDB-1:10.4.6+maria~bionic : Database - iot-dc3
 *********************************************************************
-*/
+*/
 
 /*!40101 SET NAMES utf8 */;
 
@@ -15,11 +16,6 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`iot-dc3` /*!40100 DEFAULT CHARACTER SET
 
 USE `iot-dc3`;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
 /*Table structure for table `iot_rtmp` */
 
 DROP TABLE IF EXISTS `iot_rtmp`;
@@ -30,8 +26,15 @@ CREATE TABLE `iot_rtmp` (
   `rtmp_url` varchar(128) DEFAULT NULL COMMENT 'RTMP播放链接',
   `command` varchar(128) DEFAULT NULL COMMENT 'cmd运行模板',
   `video_type` tinyint(1) DEFAULT NULL COMMENT '摄像头类型',
+  `auto_start` tinyint(1) DEFAULT NULL COMMENT '自启动',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 /*Data for the table `iot_rtmp` */
-insert  into `iot_rtmp`(`id`,`rtsp_url`,`rtmp_url`,`command`,`video_type`) values (1,'rtsp://184.72.239.149/vod/mp4://BigBuckBunny_175k.mov','rtmp://iotdc3.nginx:1935/rtmp/bigbuckbunny_175k','{exe} -i {rtsp_url} -vcodec copy -acodec copy -f flv -y {rtmp_url}',0),(2,'rtsp://admin:admin@192.168.2.236:37779/cam/realmonitor?channel=1&subtype=0','rtmp://iotdc3.nginx:1935/rtmp/monitor',NULL,1);
+
+insert  into `iot_rtmp`(`id`,`rtsp_url`,`rtmp_url`,`command`,`video_type`,`auto_start`) values (1,'rtsp://184.72.239.149/vod/mp4://BigBuckBunny_175k.mov','rtmp://iotdc3.nginx:1935/rtmp/bigbuckbunny_175k','{exe} -i {rtsp_url} -vcodec copy -acodec copy -f flv -y {rtmp_url}',0,0),(2,'rtsp://admin:admin@192.168.2.236:37779/cam/realmonitor?channel=1&subtype=0','rtmp://iotdc3.nginx:1935/rtmp/monitor',NULL,1,0);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
