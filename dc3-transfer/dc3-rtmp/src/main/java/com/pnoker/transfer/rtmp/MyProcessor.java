4931200de85c751abd27d998eb4bae63f894dc4d
pnoker <pnokers@gmail.com>
2019-07-09 17:35:29 +0800

    日常调整

--- dc3-transfer/dc3-rtmp/src/main/java/com/pnoker/transfer/rtmp/MyProcessor.java
+++
@@ -1,8 +0,0 @@
-package com.pnoker.transfer.rtmp;
-
-/**
- * @Author: lyang
- * @Date: 2019/1/1 18:17
- */
-public interface MyProcessor extends MyOutput, MyInput, MyCallback{
-}
--- dc3-transfer/dc3-rtmp/src/main/java/com/pnoker/transfer/rtmp/RtmpApplication.java
+++ dc3-transfer/dc3-rtmp/src/main/java/com/pnoker/transfer/rtmp/RtmpApplication.java
@@ -20,6 +20,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
