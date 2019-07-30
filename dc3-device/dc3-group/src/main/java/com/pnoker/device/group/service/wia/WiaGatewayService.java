be91aa68641e7cb9c4b5a312e98883caabcb5d1d
pnoker <pnokers@gmail.com>
2019-07-30 15:43:39 +0800

    gateway

---
+++ dc3-device/dc3-group/src/main/java/com/pnoker/device/group/service/wia/WiaGatewayService.java
@@ -0,0 +1,15 @@
+package com.pnoker.device.group.service.wia;
+
+import com.pnoker.device.group.model.wia.WiaGateway;
+
+import java.util.List;
+
+/**
+ * <p>Copyright(c) 2019. Pnoker All Rights Reserved.
+ * <p>Author     : Pnoker
+ * <p>Email      : pnokers@gmail.com
+ * <p>Description: WiaGateway 服务接口
+ */
+public interface WiaGatewayService {
+    List<WiaGateway> list();
+}
