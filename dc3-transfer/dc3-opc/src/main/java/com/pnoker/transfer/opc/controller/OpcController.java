f5bf8230c274ad64a9288ee6296fc57ff843217d
pnoker <pnokers@icloud.com>
2019-08-31 12:41:59 +0800

    .

--- dc3-transfer/dc3-opc/src/main/java/com/pnoker/transfer/opc/controller/OpcController.java
+++ dc3-transfer/dc3-opc/src/main/java/com/pnoker/transfer/opc/controller/OpcController.java
@@ -1,12 +1,20 @@
 package com.pnoker.transfer.opc.controller;
 
 import com.pnoker.common.base.BaseController;
+import com.pnoker.common.bean.base.ResponseBean;
 import lombok.extern.slf4j.Slf4j;
+import org.jinterop.dcom.common.JIException;
+import org.openscada.opc.dcom.list.ClassDetails;
+import org.openscada.opc.lib.list.Categories;
+import org.openscada.opc.lib.list.Category;
+import org.openscada.opc.lib.list.ServerList;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.net.UnknownHostException;
+import java.util.Collection;
 
 /**
  * <p>Copyright(c) 2019. Pnoker All Rights Reserved.
@@ -18,9 +26,38 @@ import java.util.List;
 @RestController
 public class OpcController extends BaseController {
 
-    @RequestMapping(value = "/list", method = RequestMethod.GET)
-    public String list() {
-
-        return list;
+    @RequestMapping(value = "/list/{type}", method = RequestMethod.GET)
+    public ResponseBean list(@PathVariable String type) {
+        Object object;
+        switch (type) {
+            case "server":
+                try {
+                    ServerList serverList = new ServerList(
+                            "localhost",
+                            "Administrator",
+                            "939510703",
+                            "");
+                    Collection<ClassDetails> classDetails = serverList.listServersWithDetails(new Category[]{
+                                    Categories.OPCDAServer10,
+                                    Categories.OPCDAServer20,
+                                    Categories.OPCDAServer30},
+                            new Category[]{});
+                    for (ClassDetails cds : classDetails) {
+                        System.out.println(cds.getProgId() + "=" + cds.getDescription());
+                    }
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                } catch (JIException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case "group":
+                break;
+            case "item":
+                break;
+            default:
+                return fail("This type is not supported");
+        }
+        return ok(object);
     }
 }
