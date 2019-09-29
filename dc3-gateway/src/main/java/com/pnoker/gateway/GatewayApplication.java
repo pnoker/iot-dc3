75c4b14f723111d48ff0c943b46552b5484a2996
pnoker <pnokers@gmail.com>
2019-09-29 20:17:46 +0800

    feat(gateway): add rate limiter, route configuration, controller, and hystrix fallback

--- dc3-gateway/src/main/java/com/pnoker/gateway/GatewayApplication.java
+++ dc3-gateway/src/main/java/com/pnoker/gateway/GatewayApplication.java
@@ -7,11 +7,11 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.pnoker.gateway;
@@ -20,12 +20,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-/**
- * <p>Copyright(c) 2019. Pnoker All Rights Reserved.
- * <p>@Author    : Pnoker
- * <p>Email      : pnokers@gmail.com
- * <p>Description: 平台网关服务入口
- */
 @SpringBootApplication
 public class GatewayApplication {
 
