5688da94c24d77b72edcdb90f32a9b14c9bcc7e9
pnoker <pnokers@gmail.com>
2019-10-17 21:25:25 +0800

    refactor(monitor): simplify monitor controller and add admin ui index page

--- dc3-center/dc3-monitor/src/main/java/com/pnoker/center/monitor/controller/MonitorController.java
+++ dc3-center/dc3-monitor/src/main/java/com/pnoker/center/monitor/controller/MonitorController.java
@@ -16,14 +16,9 @@
 
 package com.pnoker.center.monitor.controller;
 
-import de.codecentric.boot.admin.server.ui.extensions.UiExtension;
-import de.codecentric.boot.admin.server.ui.web.UiController;
-import de.codecentric.boot.admin.server.web.AdminController;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
-
-import java.util.List;
 
 /**
  * <p>
@@ -31,31 +26,11 @@ import java.util.List;
  * @author : pnoker
  * @email : pnokers@icloud.com
  */
-@AdminController
-public class MonitorController extends UiController {
-    private final String publicUrl;
+@Controller
+public class MonitorController {
 
-    public MonitorController(String publicUrl, String title, String brand, String favicon, String faviconDanger, List<UiExtension> uiExtensions, boolean notificationFilterEnabled, String publicUrl1) {
-        super(publicUrl, title, brand, favicon, faviconDanger, uiExtensions, notificationFilterEnabled);
-        this.publicUrl = publicUrl1;
-    }
-
-    @Override
     @ModelAttribute(value = "baseUrl", binding = false)
     public String getBaseUrl(UriComponentsBuilder uriBuilder) {
-        UriComponents publicComponents = UriComponentsBuilder.fromUriString(publicUrl).build();
-        if (publicComponents.getScheme() != null) {
-            uriBuilder.scheme(publicComponents.getScheme());
-        }
-        if (publicComponents.getHost() != null) {
-            uriBuilder.host(publicComponents.getHost());
-        }
-        if (publicComponents.getPort() != -1) {
-            uriBuilder.port(publicComponents.getPort());
-        }
-        if (publicComponents.getPath() != null) {
-            uriBuilder.path(publicComponents.getPath());
-        }
         return uriBuilder.path("/").toUriString();
     }
 }
