21d8a448c88c9f3374c86a70ca2de91b821ebb45
pnoker <pnokers@gmail.com>
2019-12-02 20:55:17 +0800

    feat(auth): add phone/email fields to User model and update seed SQL

--- dc3-api/dc3-api-dbs/src/main/java/com/pnoker/api/dbs/rtmp/feign/RtmpDbsFeignApi.java
+++ dc3-api/dc3-api-dbs/src/main/java/com/pnoker/api/dbs/rtmp/feign/RtmpDbsFeignApi.java
@@ -18,16 +18,16 @@ package com.pnoker.api.dbs.rtmp.feign;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.pnoker.api.dbs.rtmp.hystrix.RtmpDbsFeignApiHystrix;
+import com.pnoker.common.bean.Response;
 import com.pnoker.common.dto.transfer.RtmpDto;
 import com.pnoker.common.model.rtmp.Rtmp;
-import com.pnoker.common.bean.Response;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * <p>
+ * <p>Rtmp 数据 FeignApi
  *
  * @author : pnoker
  * @email : pnokers@icloud.com
--- dc3-api/dc3-api-dbs/src/main/java/com/pnoker/api/dbs/rtmp/hystrix/RtmpDbsFeignApiHystrix.java
+++ dc3-api/dc3-api-dbs/src/main/java/com/pnoker/api/dbs/rtmp/hystrix/RtmpDbsFeignApiHystrix.java
@@ -26,7 +26,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 /**
- * <p>
+ * <p>熔断
  *
  * @author : pnoker
  * @email : pnokers@icloud.com
