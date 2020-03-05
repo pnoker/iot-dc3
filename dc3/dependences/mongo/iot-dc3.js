495beb63d1e3ab43fdfd064e2bba7aac9db60b23
pnoker <pnokers@icloud.com>
2020-03-06 00:48:01 +0800

    Mongo添加线程池配合 & 支持多副本

--- dc3/dependences/mongo/iot-dc3.js
+++ dc3/dependences/mongo/iot-dc3.js
@@ -14,13 +14,25 @@
  * limitations under the License.
  */
 
-// create dc3 database and user
 db = db.getSiblingDB('dc3');
-db.createUser({
-    user: "dc3",
-    pwd: "dc3",
-    roles: [{
-        role: "readWrite",
-        db: "dc3"
-    }]
-});
+
+if (!db.getUser("dc3")) {
+    db.createUser({
+        user: "dc3",
+        pwd: "dc3",
+        roles: [{
+            role: "readWrite",
+            db: "dc3"
+        }]
+    })
+}
+
+if (!db.getCollection("pointValue")) {
+    db.createCollection("pointValue");
+    db.pointValue.createIndex({
+        "deviceId": 1,
+        "pointId": 1
+    }, {unique: false});
+}
+
+
