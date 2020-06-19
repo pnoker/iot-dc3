7762c5b81dbfd1238ab79fea7c05fccac7b3e85e
Vicky Yang <vicky-yang@icloud.com>
2020-06-20 00:17:21 +0800

    refactor(sdk): simplify logback config and improve driver common service initialisation

--- dc3-api/dc3-api-manager/src/main/java/com/dc3/api/center/manager/hystrix/PointInfoClientHystrix.java
+++ dc3-api/dc3-api-manager/src/main/java/com/dc3/api/center/manager/hystrix/PointInfoClientHystrix.java
@@ -37,7 +37,7 @@ public class PointInfoClientHystrix implements FallbackFactory<PointInfoClient> 
     @Override
     public PointInfoClient create(Throwable throwable) {
         String message = throwable.getMessage() == null ? "No available server for client: DC3-MANAGER" : throwable.getMessage();
-        log.error("Hystrix:{}", message, throwable);
+        log.error("Hystrix:{}", message);
 
         return new PointInfoClient() {
 
