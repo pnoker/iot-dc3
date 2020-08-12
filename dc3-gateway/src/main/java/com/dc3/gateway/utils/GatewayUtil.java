ce5a2ff461b1fa5b7298095381d32d8ee0d99aef
pnoker <pnokers@icloud.com>
2020-08-12 23:51:05 +0800

    #bugfix gateway authentic

--- dc3-gateway/src/main/java/com/dc3/gateway/utils/GatewayUtil.java
+++ dc3-gateway/src/main/java/com/dc3/gateway/utils/GatewayUtil.java
@@ -16,6 +16,8 @@
 
 package com.dc3.gateway.utils;
 
+import com.dc3.common.exception.ServiceException;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 
 import java.util.Objects;
@@ -28,8 +30,8 @@ public class GatewayUtil {
     /**
      * 获取远程客户端 IP
      *
-     * @param request
-     * @return
+     * @param request ServerHttpRequest
+     * @return Remote Ip
      */
     public static String getRemoteIp(ServerHttpRequest request) {
         String ip = request.getHeaders().getFirst("x-forwarded-for");
@@ -46,5 +48,20 @@ public class GatewayUtil {
             ip = Objects.requireNonNull(request.getRemoteAddress()).getHostString();
         }
         return ip;
+    }
+
+    /**
+     * 获取 Request Header
+     *
+     * @param request ServerHttpRequest
+     * @param key     header key
+     * @return request header value
+     */
+    public static String getRequestHeader(ServerHttpRequest request, String key) {
+        String header = request.getHeaders().getFirst(key);
+        if (!StringUtils.isNotBlank(header)) {
+            throw new ServiceException("Invalid request header of " + key);
+        }
+        return header;
     }
 }
