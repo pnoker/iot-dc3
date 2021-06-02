71a1d208f0cc9a3b6500b0542ce7233010a4a165
pnoker <pnokers@gmail.com>
2021-06-02 11:54:36 +0800

    feat(manager): add separate status query endpoints for device and driver

--- dc3-center/dc3-manager/src/main/java/com/dc3/center/manager/service/StatusService.java
+++ dc3-center/dc3-manager/src/main/java/com/dc3/center/manager/service/StatusService.java
@@ -26,7 +26,15 @@ import java.util.Map;
 public interface StatusService {
 
     /**
-     * 查询 Driver 服务状态
+     * 根据 驱动ServiceName 查询 Driver 服务状态
+     *
+     * @param serviceName Driver ServiceName
+     * @return String
+     */
+    String driver(String serviceName);
+
+    /**
+     * 分页查询 Driver 服务状态，同驱动分页查询配套使用
      *
      * @param driverDto Driver Dto
      * @return Map<String, String>
@@ -34,7 +42,15 @@ public interface StatusService {
     Map<Long, String> driver(DriverDto driverDto);
 
     /**
-     * 查询 Device 服务状态
+     * 根据 设备Id 查询 Device 服务状态
+     *
+     * @param id Device Id
+     * @return String
+     */
+    String device(Long id);
+
+    /**
+     * 分页查询 Device 服务状态，同设备分页查询配套使用
      *
      * @param deviceDto Device Dto
      * @return Map<Long, String>
