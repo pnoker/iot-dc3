1a3fac21b803585b280f64cc1a275e9643b0f19d
pnoker <pnokers@gmail.com>
2021-08-15 17:18:46 +0800

    refactor(common): rename fields across Device, Driver, Group, Point, Profile and DevicePoint models

--- dc3-common/dc3-base/src/main/java/com/dc3/common/model/Point.java
+++ dc3-common/dc3-base/src/main/java/com/dc3/common/model/Point.java
@@ -40,7 +40,7 @@ import javax.validation.constraints.Pattern;
 public class Point extends Description {
 
     @NotBlank(message = "name can't be empty", groups = {Insert.class})
-    @Pattern(regexp = "^[A-Za-z0-9\\u4e00-\\u9fa5][A-Za-z0-9\\u4e00-\\u9fa5-_.]{1,31}$", message = "invalid name,contains invalid characters or length is not in the range of 2~32", groups = {Insert.class, Update.class})
+    @Pattern(regexp = "^[A-Za-z0-9\\u4e00-\\u9fa5][A-Za-z0-9\\u4e00-\\u9fa5-_#@/\\.\\|]{1,31}$", message = "invalid name,contains invalid characters or length is not in the range of 2~32", groups = {Insert.class, Update.class})
     private String name;
 
     private String type = Common.ValueType.STRING;
