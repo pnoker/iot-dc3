dacf41d02f1fcefa7edb43927aaf28e274a7dbfd
pnoker <pnokers@gmail.com>
2022-06-22 01:05:30 +0800

    feat: update version

--- src/config/plugins/index.js
+++ src/config/plugins/index.js
@@ -3,14 +3,12 @@ import '@/config/plugins/highlight/highlight.js';
 import '@/config/plugins/map/baidu-map.js';
 
 console.log(
-    '.___     ___________ ________  _________ ________    __      __      ___.    \n' +
-    '|   | ___\\__    ___/ \\______ \\ \\_   ___ \\\\_____  \\  /  \\    /  \\ ____\\_ |__  \n' +
-    '|   |/  _ \\|    |     |    |  \\/    \\  \\/  _(__  <  \\   \\/\\/   // __ \\| __ \\ \n' +
-    '|   (  <_> )    |     |    `   \\     \\____/       \\  \\        /\\  ___/| \\_\\ \\\n' +
-    '|___|\\____/|____|    /_______  /\\______  /______  /   \\__/\\  /  \\___  >___  /\n' +
-    '                             \\/        \\/       \\/         \\/       \\/    \\/ \n' +
+    '.___     ___________ ________  _________ ________\n' +
+    '|   | ___\\__    ___/ \\______ \\ \\_   ___ \\\\_____  \\\n' +
+    '|   |/  _ \\|    |     |    |  \\/    \\  \\/  _(__  <\n' +
+    '|   (  <_> )    |     |    `   \\     \\____/       \\\n' +
+    '|___|\\____/|____|    /_______  /\\______  /______  /\n' +
+    '                             \\/        \\/       \\/\n' +
     'https://doc.dc3.site\n' +
-    'https://gitee.com/pnoker/iot-dc3\n' +
-    'https://github.com/pnoker/iot-dc3\n' +
-    'DC3 IOT Platform V1.3.2.SR Pnoker Authors'
+    '${AnsiColor.BRIGHT_CYAN}DC3 IOT Platform V2022.1.0 ${AnsiColor.BRIGHT_YELLOW}Pnoker${AnsiColor.BRIGHT_CYAN} Authors'
 );
