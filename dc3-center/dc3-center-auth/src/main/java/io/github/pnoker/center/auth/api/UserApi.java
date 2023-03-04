43572784db6a57693b6526f316e2b144fa41d741
pnoker <pnokers@gmail.com>
2023-03-05 01:30:55 +0800

    feat(auth): add UserApi gRPC proto and restructure api pom dependencies

---
+++ dc3-center/dc3-center-auth/src/main/java/io/github/pnoker/center/auth/api/UserApi.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2016-present Pnoker All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.pnoker.center.auth.api;
+
+
+import cn.hutool.core.util.ObjectUtil;
+import io.github.pnoker.api.center.auth.NameQuery;
+import io.github.pnoker.api.center.auth.RUser;
+import io.github.pnoker.api.center.auth.UserApiGrpc;
+import io.github.pnoker.api.common.EnableFlagEnum;
+import io.github.pnoker.api.common.R;
+import io.github.pnoker.center.auth.service.UserService;
+import io.github.pnoker.common.enums.ResponseEnum;
+import io.github.pnoker.common.model.User;
+import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+import javax.annotation.Resource;
+
+/**
+ * User Api
+ *
+ * @author pnoker
+ * @since 2022.1.0
+ */
+@Slf4j
+@GrpcService
+public class UserApi extends UserApiGrpc.UserApiImplBase {
+
+    @Resource
+    private UserService userService;
+
+    @Override
+    public void selectByName(NameQuery request, StreamObserver<RUser> responseObserver) {
+        RUser.Builder builder = RUser.newBuilder();
+        R.Builder rBuilder = R.newBuilder();
+        builder.setResult(rBuilder);
+        User select = userService.selectByLoginName(request.getName(), false);
+        if (ObjectUtil.isNotNull(select)) {
+            rBuilder.setOk(false);
+            rBuilder.setCode(ResponseEnum.NO_RESOURCE.getCode());
+            rBuilder.setMessage(ResponseEnum.NO_RESOURCE.getMessage());
+        } else {
+            rBuilder.setOk(true);
+            rBuilder.setCode(ResponseEnum.OK.getCode());
+            rBuilder.setMessage(ResponseEnum.OK.getMessage());
+            io.github.pnoker.api.center.auth.User user = buildDTOByDO(select);
+            builder.setData(user);
+        }
+
+        responseObserver.onNext(builder.build());
+        responseObserver.onCompleted();
+    }
+
+
+    private io.github.pnoker.api.center.auth.User buildDTOByDO(User entityDO) {
+        io.github.pnoker.api.center.auth.User.Builder builder = io.github.pnoker.api.center.auth.User.newBuilder();
+        builder.setLoginName(entityDO.getLoginName());
+        builder.setUserExtId(entityDO.getUserExtId());
+        builder.setUserPasswordId(entityDO.getUserPasswordId());
+        builder.setEnableFlag(EnableFlagEnum.valueOf(entityDO.getEnableFlag().name()));
+        return builder.build();
+    }
+}
