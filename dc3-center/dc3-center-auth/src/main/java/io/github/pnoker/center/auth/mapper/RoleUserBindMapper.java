27cf769293d2283855df158ab295cdea289495a8
linyousheng <810275303@qq.com>
2023-04-02 23:05:01 +0800

    feat: auth提交代码，增加UserManageService

---
+++ dc3-center/dc3-center-auth/src/main/java/io/github/pnoker/center/auth/mapper/RoleUserBindMapper.java
@@ -0,0 +1,13 @@
+package io.github.pnoker.center.auth.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import io.github.pnoker.common.model.RoleUserBind;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author linys
+ * @since 2023.04.02
+ */
+@Mapper
+public interface RoleUserBindMapper extends BaseMapper<RoleUserBind> {
+}
