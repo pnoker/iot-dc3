3c4167f9cda5c587b785bdd3493a28aef2499a8a
pnoker <pnokers@icloud.com>
2023-04-14 01:00:21 +0800

    feat: add CNAME:doc.dc3.site

---
+++ docs/assets/js/push.js
@@ -0,0 +1,17 @@
+var _hmt = _hmt || []
+;(function () {
+    var bp = document.createElement('script')
+    var curProtocol = window.location.protocol.split(':')[0]
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js'
+    } else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js'
+    }
+    var s = document.getElementsByTagName('script')[0]
+    s.parentNode.insertBefore(bp, s)
+
+    var hm = document.createElement('script')
+    hm.src = 'https://hm.baidu.com/hm.js?d59fbe5e86cf5c5a0929db6d1ec5e9c0'
+    var s = document.getElementsByTagName('script')[0]
+    s.parentNode.insertBefore(hm, s)
+})()
