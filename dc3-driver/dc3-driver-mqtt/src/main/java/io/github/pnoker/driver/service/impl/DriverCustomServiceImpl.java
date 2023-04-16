d70c8226096ff7942e7b0eb1e026e9b78c9f1c0b
pnoker <pnokers@gmail.com>
2023-04-16 22:05:42 +0800

    feat: update driver custom service

--- dc3-driver/dc3-driver-mqtt/src/main/java/io/github/pnoker/driver/service/impl/DriverCustomServiceImpl.java
+++ dc3-driver/dc3-driver-mqtt/src/main/java/io/github/pnoker/driver/service/impl/DriverCustomServiceImpl.java
@@ -18,7 +18,7 @@ package io.github.pnoker.driver.service.impl;
 
 import io.github.pnoker.common.constant.common.DefaultConstant;
 import io.github.pnoker.common.entity.driver.AttributeInfo;
-import io.github.pnoker.common.enums.DriverStatusEnum;
+import io.github.pnoker.common.enums.DeviceStatusEnum;
 import io.github.pnoker.common.model.Device;
 import io.github.pnoker.common.model.Point;
 import io.github.pnoker.driver.sdk.DriverContext;
@@ -50,17 +50,48 @@ public class DriverCustomServiceImpl implements DriverCustomService {
 
     @Override
     public void initial() {
+        /*
+        !!! 提示：此处逻辑仅供参考，请务必结合实际应用场景。!!!
+        !!!
+        你可以在此处执行一些特定的初始化逻辑，驱动在启动的时候会自动执行该方法。
+        */
+    }
+
+    @Override
+    public void schedule() {
+        /*
+        !!! 提示：此处逻辑仅供参考，请务必结合实际应用场景。!!!
+        !!!
+        上传设备状态，可自行灵活拓展，不一定非要在schedule()接口中实现，你可以：
+        - 在read中实现设备状态的判断；
+        - 在自定义定时任务中实现设备状态的判断；
+        - 通过某种判断机制实现设备状态的判断。
+
+        最后通过 driverSenderService.deviceStatusSender(deviceId,deviceStatus) 接口将设备状态交给SDK管理，其中设备状态（StatusEnum）：
+        - ONLINE:在线
+        - OFFLINE:离线
+        - MAINTAIN:维护
+        - FAULT:故障
+         */
+        driverContext.getDriverMetadata().getDeviceMap().keySet().forEach(id -> driverSenderService.deviceStatusSender(id, DeviceStatusEnum.ONLINE));
     }
 
     @Override
     public String read(Map<String, AttributeInfo> driverInfo, Map<String, AttributeInfo> pointInfo, Device device, Point point) {
-        // 因为 MQTT 的数据来源是被动接收的，所以无需实现该 Read 方法
-        // 接收数据处理函数在 io.github.pnoker.common.mqtt.handler.MqttReceiveHandler.handlerValue
+        /*
+        !!! 提示：此处逻辑仅供参考，请务必结合实际应用场景。!!!
+
+        因为 MQTT 的数据来源是被动接收的，所以无需实现该 Read 方法
+        接收数据处理函数在 io.github.pnoker.common.mqtt.handler.MqttReceiveHandler.handlerValue
+         */
         return DefaultConstant.DEFAULT_VALUE;
     }
 
     @Override
     public Boolean write(Map<String, AttributeInfo> driverInfo, Map<String, AttributeInfo> pointInfo, Device device, AttributeInfo values) {
+        /*
+        !!! 提示：此处逻辑仅供参考，请务必结合实际应用场景。!!!
+         */
         String commandTopic = attribute(pointInfo, "commandTopic");
         String value = values.getValue();
         try {
@@ -70,23 +101,6 @@ public class DriverCustomServiceImpl implements DriverCustomService {
             //mqttSendService.sendToMqtt(commandTopic, value);
         }
         return true;
-    }
-
-    @Override
-    public void schedule() {
-
-        /*
-        TODO:设备状态
-        上传设备状态，可自行灵活拓展，不一定非要在schedule()接口中实现，也可以在read中实现设备状态的设置；
-        你可以通过某种判断机制确定设备的状态，然后通过driverService.deviceEventSender接口将设备状态交给SDK管理。
-
-        设备状态（StatusEnum）如下：
-        ONLINE:在线
-        OFFLINE:离线
-        MAINTAIN:维护
-        FAULT:故障
-         */
-        driverContext.getDriverMetadata().getDeviceMap().keySet().forEach(id -> driverSenderService.deviceStatusSender(id, DriverStatusEnum.ONLINE));
     }
 
 }
