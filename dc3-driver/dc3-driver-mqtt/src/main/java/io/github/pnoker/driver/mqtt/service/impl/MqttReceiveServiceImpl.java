f3db2bbdb81a4df4bf348c949acdb9952ef28b28
xwh201314 <981800006@qq.com>
2023-04-17 15:50:51 +0800

    fix(driver): set collect time when missing in mqtt demo receive service

--- dc3-driver/dc3-driver-mqtt/src/main/java/io/github/pnoker/driver/mqtt/service/impl/MqttReceiveServiceImpl.java
+++ dc3-driver/dc3-driver-mqtt/src/main/java/io/github/pnoker/driver/mqtt/service/impl/MqttReceiveServiceImpl.java
@@ -25,6 +25,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -44,6 +45,7 @@ public class MqttReceiveServiceImpl implements MqttReceiveService {
         // do something to process your mqtt messages
         log.info(JsonUtil.toPrettyJsonString(mqttMessage));
         PointValue pointValue = JsonUtil.parseObject(mqttMessage.getPayload(), PointValue.class);
+        pointValue.setOriginTime(new Date());
         driverSenderService.pointValueSender(pointValue);
     }
 
