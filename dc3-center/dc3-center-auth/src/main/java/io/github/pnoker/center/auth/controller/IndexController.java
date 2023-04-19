58effe33f92759600ea0d07a796b1f89757134f5
pnoker <pnokers@gmail.com>
2023-04-19 19:31:13 +0800

    fix: remove ping

--- dc3-center/dc3-center-auth/src/main/java/io/github/pnoker/center/auth/controller/IndexController.java
+++ dc3-center/dc3-center-auth/src/main/java/io/github/pnoker/center/auth/controller/IndexController.java
@@ -34,7 +34,7 @@ import java.util.Date;
  */
 @Slf4j
 @RestController
-@RequestMapping(value = AuthServiceConstant.INDEX_URL_PREFIX)
+//@RequestMapping(value = AuthServiceConstant.INDEX_URL_PREFIX)
 public class IndexController {
 
     /**
@@ -42,9 +42,9 @@ public class IndexController {
      *
      * @return DataTime
      */
-    @GetMapping("/ping")
+    /*@GetMapping("/ping")
     public R<String> ping() {
         return R.ok(TimeUtil.defaultFormat(new Date()));
-    }
+    }*/
 
 }
--- dc3-center/dc3-center-manager/src/main/java/io/github/pnoker/center/manager/controller/IndexController.java
+++ dc3-center/dc3-center-manager/src/main/java/io/github/pnoker/center/manager/controller/IndexController.java
@@ -35,7 +35,7 @@ import java.util.Date;
  */
 @Slf4j
 @RestController
-@RequestMapping(value = ManagerServiceConstant.INDEX_URL_PREFIX)
+//@RequestMapping(value = ManagerServiceConstant.INDEX_URL_PREFIX)
 public class IndexController {
 
     /**
@@ -43,9 +43,9 @@ public class IndexController {
      *
      * @return DataTime
      */
-    @GetMapping("/ping")
+    /*@GetMapping("/ping")
     public R<String> ping() {
         return R.ok(TimeUtil.defaultFormat(new Date()));
-    }
+    }*/
 
 }
