3965a23ea033e0a1d696756237a7fd0af27653be
xwh201314 <981800006@qq.com>
2023-06-21 17:41:46 +0800

    feat(manager): add node-red rule engine controller, service, and mappers

---
+++ dc3-center/dc3-center-manager/src/main/java/io/github/pnoker/center/manager/mapper/NodeRedCredentialsMapper.java
@@ -0,0 +1,9 @@
+package io.github.pnoker.center.manager.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import io.github.pnoker.common.model.NodeRedCredentials;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface NodeRedCredentialsMapper extends BaseMapper<NodeRedCredentials> {
+}
---
+++ dc3-center/dc3-center-manager/src/main/java/io/github/pnoker/center/manager/mapper/NodeRedLibraryMapper.java
@@ -0,0 +1,9 @@
+package io.github.pnoker.center.manager.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import io.github.pnoker.common.model.NodeRedLibrary;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface NodeRedLibraryMapper extends BaseMapper<NodeRedLibrary> {
+}
