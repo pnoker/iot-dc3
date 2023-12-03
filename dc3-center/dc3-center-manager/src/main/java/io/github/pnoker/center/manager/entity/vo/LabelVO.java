0d3c4d3efdeaefcfd7eb0255d78a2760f0bf18f0
pnoker <pnokers@icloud.com>
2023-12-04 02:00:57 +0800

    feat: .

--- dc3-center/dc3-center-manager/src/main/java/io/github/pnoker/center/manager/entity/vo/LabelVO.java
+++ dc3-center/dc3-center-manager/src/main/java/io/github/pnoker/center/manager/entity/vo/LabelVO.java
@@ -1,8 +1,5 @@
 package io.github.pnoker.center.manager.entity.vo;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
 import java.io.Serializable;
