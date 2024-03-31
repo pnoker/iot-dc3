0966f3d3dd7091f86c25d81dc9626d0f553aded2
ennian001 <sunennian@sina.com>
2024-03-31 18:56:16 +0800

    fix(manager): remove stray @JsonFormat annotation from DeviceDO id field

--- dc3-center/dc3-center-manager/src/main/java/io/github/pnoker/center/manager/entity/model/DeviceDO.java
+++ dc3-center/dc3-center-manager/src/main/java/io/github/pnoker/center/manager/entity/model/DeviceDO.java
@@ -18,7 +18,6 @@ package io.github.pnoker.center.manager.entity.model;
 
 import com.baomidou.mybatisplus.annotation.*;
 import com.baomidou.mybatisplus.extension.handlers.JacksonTypeHandler;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import io.github.pnoker.common.entity.ext.JsonExt;
 import lombok.Getter;
 import lombok.Setter;
@@ -44,7 +43,6 @@ public class DeviceDO implements Serializable {
     /**
      * 主键ID
      */
-    @JsonFormat(shape = JsonFormat.Shape.STRING)
     @TableId(value = "id", type = IdType.ASSIGN_ID)
     private Long id;
 
@@ -63,7 +61,6 @@ public class DeviceDO implements Serializable {
     /**
      * 驱动ID
      */
-    @JsonFormat(shape = JsonFormat.Shape.STRING)
     @TableField("driver_id")
     private Long driverId;
 
