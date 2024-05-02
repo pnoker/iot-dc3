6532681a4eab3852f8a94ad723fa83aba1b140d3
zzi666 <756106614@qq.com>
2024-05-02 14:14:25 +0800

    feat: 修复一个分页查询 Bug

--- dc3-center/dc3-center-manager/src/main/java/io/github/pnoker/center/manager/service/impl/PointServiceImpl.java
+++ dc3-center/dc3-center-manager/src/main/java/io/github/pnoker/center/manager/service/impl/PointServiceImpl.java
@@ -271,10 +271,10 @@ public class PointServiceImpl implements PointService {
         if (ObjectUtil.isNotEmpty(bindDO)) {
             List<PointDO> list = pointManager.list(new LambdaQueryWrapper<PointDO>().eq(PointDO::getProfileId, bindDO.getProfileId()));
             if (ObjectUtil.isNotEmpty(list)) {
-                List<Long> profileList = list.stream().map(e -> e.getId()).toList();
+                List<Long> profileList = list.stream().map(PointDO::getId).toList();
                 List<PointAttributeConfigDO> list2 = pointAttributeConfigManager.list(new LambdaQueryWrapper<PointAttributeConfigDO>().eq(PointAttributeConfigDO::getDeviceId, deviceId));
                 if (ObjectUtil.isNotEmpty(list2)) {
-                    List<Long> attrList = list2.stream().map(e -> e.getPointId()).toList();
+                    List<Long> attrList = new ArrayList<>(list2.stream().map(PointAttributeConfigDO::getPointId).toList());
                     //取交集
                     attrList.retainAll(profileList);
                     configCount.addAll(attrList);
