19483c97c502e7fef3513c1ab6adf27fe0c8e7b6
嘉平十二 <2441319121@qq.com>
2024-05-15 18:06:18 +0800

    feat:优化

--- dc3-center/dc3-center-manager/src/main/java/io/github/pnoker/center/manager/entity/query/TopicQuery.java
+++ dc3-center/dc3-center-manager/src/main/java/io/github/pnoker/center/manager/entity/query/TopicQuery.java
@@ -1,7 +1,6 @@
 package io.github.pnoker.center.manager.entity.query;
 
 import io.github.pnoker.common.entity.common.Pages;
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 import java.io.Serial;
@@ -18,18 +17,16 @@ import java.io.Serializable;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Schema(title = "TopicQuery", description = "主题-查询")
 public class TopicQuery implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
-    @Schema(description = "分页")
     private Pages page;
 
     /**
      * 主题
      */
-    @Schema(description = "主题")
+
     private String topic;
 
     // 查询字段
@@ -37,6 +34,6 @@ public class TopicQuery implements Serializable {
     /**
      * 设备名称
      */
-    @Schema(description = "设备名称")
+
     private String deviceName;
 }
