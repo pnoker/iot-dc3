0cf70ef0ef40397d13a9bbf625105a77f0c9cbb0
嘉平十二 <2441319121@qq.com>
2024-05-16 12:51:30 +0800

    refactor(manager): simplify TopicController list endpoint

--- dc3-center/dc3-center-manager/src/main/java/io/github/pnoker/center/manager/controller/TopicController.java
+++ dc3-center/dc3-center-manager/src/main/java/io/github/pnoker/center/manager/controller/TopicController.java
@@ -25,10 +25,7 @@ import io.github.pnoker.common.base.BaseController;
 import io.github.pnoker.common.constant.service.ManagerConstant;
 import io.github.pnoker.common.entity.R;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
