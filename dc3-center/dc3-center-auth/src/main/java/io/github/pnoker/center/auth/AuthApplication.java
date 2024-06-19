4db3db2a6d90de938357bcf1eab8bb0dae91add5
pnoker <pnokers@gmail.com>
2024-06-19 23:21:41 +0800

    refactor(auth): extract auth service business code into dc3-common-auth module

--- dc3-center/dc3-center-auth/src/main/java/io/github/pnoker/center/auth/AuthApplication.java
+++ dc3-center/dc3-center-auth/src/main/java/io/github/pnoker/center/auth/AuthApplication.java
@@ -16,10 +16,8 @@
 
 package io.github.pnoker.center.auth;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * 授权中心服务启动入口
@@ -28,8 +26,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  * @since 2022.1.0
  */
 @SpringBootApplication
-@EnableTransactionManagement
-@MapperScan("io.github.pnoker.center.auth.mapper")
 public class AuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
