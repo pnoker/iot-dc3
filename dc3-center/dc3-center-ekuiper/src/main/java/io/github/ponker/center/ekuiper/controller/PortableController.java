fb53bc01d1e3c2b7df51597bced7bfe78ad45132
最终抚慰 <1756065426@qq.com>
2024-08-23 14:09:50 +0800

    第二次对文件上传的修改

--- dc3-center/dc3-center-ekuiper/src/main/java/io/github/ponker/center/ekuiper/controller/PortableController.java
+++ dc3-center/dc3-center-ekuiper/src/main/java/io/github/ponker/center/ekuiper/controller/PortableController.java
@@ -1,5 +1,6 @@
 package io.github.ponker.center.ekuiper.controller;
 
+import cn.hutool.core.io.FileUtil;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.github.ponker.center.ekuiper.constant.ServiceConstant;
 import io.github.ponker.center.ekuiper.entity.R;
@@ -8,24 +9,18 @@ import io.github.ponker.center.ekuiper.service.ApiService;
 import io.github.ponker.center.ekuiper.service.PortableService;
 import io.github.ponker.center.ekuiper.service.UrlService;
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.util.FileUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.codec.multipart.FilePart;
-import org.springframework.http.codec.multipart.Part;
-import org.springframework.util.FileCopyUtils;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.io.*;
-import java.nio.Buffer;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Slf4j
 @RestController
@@ -37,10 +32,12 @@ public class PortableController {
     private PortableService portableService;
     @Autowired
     private ApiService apiService;
+    @Value("${driver.custom.FILE_PATH}")
+    private String uploadFilePath;
     @GetMapping("/list")
     public Mono<R<Page<PortableDataVO>>> getAllPortables(@RequestParam(required = false, defaultValue = "1") Integer pageNum,
-                                                         @RequestParam(required = false, defaultValue = "10") Integer pageSize){
-        Mono<Page<PortableDataVO>> pageMono=portableService.callApiWithPortableData(HttpMethod.GET,urlService.getPortableUrl(),pageNum,pageSize);
+                                                         @RequestParam(required = false, defaultValue = "10") Integer pageSize) {
+        Mono<Page<PortableDataVO>> pageMono = portableService.callApiWithPortableData(HttpMethod.GET, urlService.getPortableUrl(), pageNum, pageSize);
         return pageMono.flatMap(s -> {
             try {
                 return Mono.just(R.ok(s, "successful"));
@@ -51,10 +48,11 @@ public class PortableController {
         });
 
     }
+
     @GetMapping("/getOne/{name}")
-    public Mono<R<PortableDataVO>> getOnePortable(@PathVariable String name){
-        String url=urlService.getPortableUrl()+"/"+name;
-        Mono<PortableDataVO> portableDataVOMono=portableService.callApiWithPortable(HttpMethod.GET,url);
+    public Mono<R<PortableDataVO>> getOnePortable(@PathVariable String name) {
+        String url = urlService.getPortableUrl() + "/" + name;
+        Mono<PortableDataVO> portableDataVOMono = portableService.callApiWithPortable(HttpMethod.GET, url);
         return portableDataVOMono.flatMap(s -> {
             try {
                 return Mono.just(R.ok(s, "successful"));
@@ -69,51 +67,39 @@ public class PortableController {
 
     @PostMapping(value = "/upload/{name}")
     public Mono<R<String>> uploadFile(@RequestPart("uploadFile") Mono<FilePart> filePartMono,
-                                      @PathVariable  String name) {
-        String url= urlService.getUploadFileUrl();
-        Mono<String> stringMono = apiService.callApiWithFile(filePartMono, HttpMethod.POST, url);
-        return stringMono.flatMap(s -> {
-            try {
-                File file=new File(s);
-                String[] uri = s.split("/");
-                String relUrl="";
-                for (int i=0;i<uri.length-1;i++){
-                    relUrl+=uri[i]+"/";
-                }
-                relUrl=relUrl+name;
-                file.renameTo(new File(relUrl));
-                return Mono.just(R.ok("file://"+relUrl));
-            } catch (Exception e) {
-                log.error("Failed to call create Portable API", e);
-                return Mono.just(R.fail(e.getMessage()));
-            }
+                                      @PathVariable String name) {
+        return filePartMono.flatMap(filePart -> {
+            String filePath = uploadFilePath+"/" + name;
+            File file = new File(filePath);
+            return filePart.transferTo(file).then(Mono.defer(() -> {
+                return Mono.just(R.ok(filePath));
+            }));
         });
     }
+
     @PostMapping(value = "/uploadFile")
     public Mono<R<String>> uploadFileByBlob(@RequestPart("uploadFile") Mono<FilePart> filePartMono) {
-        String url= urlService.getUploadFileUrl();
+        String url = urlService.getUploadFileUrl();
         Mono<String> stringMono = apiService.callApiWithFile(filePartMono, HttpMethod.POST, url);
         return stringMono.flatMap(s -> {
             try {
-                return Mono.just(R.ok("file://"+s));
+                return Mono.just(R.ok("file://" + s));
             } catch (Exception e) {
                 log.error("Failed to call create Portable API", e);
                 return Mono.just(R.fail(e.getMessage()));
             }
         });
     }
-    @PostMapping(value = "/uploadPortable")
-    public Mono<R<String>> uploadMuFile(@RequestBody MultipartFile uploadFile) {
-        String url= urlService.getUploadFileUrl();
-        Mono<String> stringMono = apiService.callApiWithMuFile(uploadFile, HttpMethod.POST, url);
-        return stringMono.flatMap(s -> {
-            try {
-                return Mono.just(R.ok("file://"+s));
-            } catch (Exception e) {
-                log.error("Failed to call create Portable API", e);
-                return Mono.just(R.fail(e.getMessage()));
-            }
-        });
+
+    @PostMapping(value = "/uploadPortable/{name}")
+    public Mono<R<String>> uploadMuFile(@RequestPart("uploadFile") Flux<FilePart> filePartFlux, @PathVariable String name) {
+        String filePath = uploadFilePath;
+        return filePartFlux.flatMap(file -> {
+            Mono<Void> voidMono = file.transferTo(Paths.get(filePath, file.filename()));
+            return voidMono.then(Mono.just(file.filename()));
+        }).then(Mono.just(R.ok(
+                filePath + "/" + name
+        )));
     }
 
 
@@ -129,9 +115,10 @@ public class PortableController {
             }
         });
     }
+
     @DeleteMapping("/delete/{name}")
-    public Mono<R<String>> deletePortable(@PathVariable String name){
-        String url=urlService.getPortableUrl()+"/"+name;
+    public Mono<R<String>> deletePortable(@PathVariable String name) {
+        String url = urlService.getPortableUrl() + "/" + name;
         Mono<String> stringMono = apiService.callApi(HttpMethod.DELETE, url);
         return stringMono.flatMap(s -> {
             try {
@@ -143,9 +130,10 @@ public class PortableController {
         });
 
     }
+
     @PutMapping("/update/{name}")
-    public Mono<R<String>> updatePortable(@Validated @RequestBody Object form,@PathVariable String name){
-        String url=urlService.getPortableUrl()+"/"+name;
+    public Mono<R<String>> updatePortable(@Validated @RequestBody Object form, @PathVariable String name) {
+        String url = urlService.getPortableUrl() + "/" + name;
         Mono<String> stringMono = apiService.callApiWithData(form, HttpMethod.PUT, url);
         return stringMono.flatMap(s -> {
             try {
