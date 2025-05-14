ac20257dcfea1d7f15a64c4826b26a3d1d4755ad
Vickey <891607480@qq.com>
2025-05-14 15:55:40 +0800

    feat: .

---
+++ dc3-common/dc3-common-data/src/main/java/io/github/pnoker/common/data/entity/model/DeviceEventDO.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2016-present the IoT DC3 original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.pnoker.common.data.entity.model;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+/**
+ * <p>
+ * 设备事件表
+ * </p>
+ *
+ * @author pnoker
+ * @version 2025.2.5
+ * @since 2022.1.0
+ */
+@Getter
+@Setter
+@ToString
+@TableName("dc3_device_event")
+public class DeviceEventDO implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 主键ID
+     */
+    @TableId(value = "id", type = IdType.ASSIGN_ID)
+    private Long id;
+
+    /**
+     * 设备ID
+     */
+    @TableField("device_id")
+    private Long deviceId;
+
+    /**
+     * 位号ID
+     */
+    @TableField("point_id")
+    private Long pointId;
+
+    /**
+     * 事件类型标识
+     */
+    @TableField("event_type_flag")
+    private Byte eventTypeFlag;
+
+    /**
+     * 事件相关信息
+     */
+    @TableField("event_ext")
+    private Object eventExt;
+
+    /**
+     * 过期时长, 秒
+     */
+    @TableField("expired_time")
+    private Long expiredTime;
+
+    /**
+     * 确认标识, 0:未确认, 1:已确认
+     */
+    @TableField("confirm_flag")
+    private Byte confirmFlag;
+
+    /**
+     * 创建时间
+     */
+    @TableField("create_time")
+    private LocalDateTime createTime;
+
+    /**
+     * 操作时间
+     */
+    @TableField("operate_time")
+    private LocalDateTime operateTime;
+
+    /**
+     * 逻辑删除标识, 0:未删除, 1:已删除
+     */
+    @TableLogic
+    @TableField("deleted")
+    private Byte deleted;
+}
