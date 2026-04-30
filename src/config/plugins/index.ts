e4549c5a79fcba12849fbb54ee3ef15add6fe1e9
pnoker <pnokers@gmail.com>
2026-04-30 15:56:04 +0800

    chore: bump version to 2026.4.30

--- src/config/plugins/index.ts
+++ src/config/plugins/index.ts
@@ -29,7 +29,7 @@ const PLATFORM_BANNER = `
 |___|\\\\____/|____|    /_______  /\\\\______  /______  /
                              \\\\/        \\\\/       \\\\/
 https://doc.dc3.site
-IoT DC3 Platform V2026.4.29`;
+IoT DC3 Platform V2026.4.30`;
 
 /**
  * Plugin setup function
