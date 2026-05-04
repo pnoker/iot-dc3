95aec4f37ff60a4857dc483612c4870f683c566a
pnoker <pnokers@gmail.com>
2026-05-04 21:00:27 +0800

    chore: collapse imports and tidy TimeRangeUtil javadoc

--- dc3-common/dc3-common-auth/src/main/java/io/github/pnoker/common/auth/service/impl/RoleServiceImpl.java
+++ dc3-common/dc3-common-auth/src/main/java/io/github/pnoker/common/auth/service/impl/RoleServiceImpl.java
@@ -38,12 +38,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 /**
  * <p>
