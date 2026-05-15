9437934cabb2d85c5e187e5e8ec472fde35ad874
pnoker <pnokers@gmail.com>
2026-05-15 19:54:45 +0800

    refactor(common): improve postgres and repository modules

---
+++ dc3-common/dc3-common-postgres/src/test/java/io/github/pnoker/common/config/MybatisPlusConfigTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2016-present the IoT DC3 original author or authors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package io.github.pnoker.common.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MybatisPlusConfigTest {
+
+    private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+            .withConfiguration(AutoConfigurations.of(MybatisPlusConfig.class));
+
+    @Test
+    void mybatisPlusInterceptorIsCreatedByDefault() {
+        contextRunner.run(context -> assertThat(context).hasSingleBean(MybatisPlusInterceptor.class));
+    }
+
+    @Test
+    void mybatisPlusInterceptorBacksOffWhenUserBeanExists() {
+        MybatisPlusInterceptor customInterceptor = new MybatisPlusInterceptor();
+
+        contextRunner.withBean(MybatisPlusInterceptor.class, () -> customInterceptor)
+                .run(context -> assertThat(context.getBean(MybatisPlusInterceptor.class)).isSameAs(customInterceptor));
+    }
+
+}
