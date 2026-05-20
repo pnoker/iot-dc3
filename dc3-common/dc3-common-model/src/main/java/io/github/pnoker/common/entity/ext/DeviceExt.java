cc48435852111176785223eb062cbae8651d49a0
pnoker <pnokers@gmail.com>
2026-05-21 01:06:31 +0800

    docs(model): fix remaining empty Javadoc on Ext classes

--- dc3-common/dc3-common-model/src/main/java/io/github/pnoker/common/entity/ext/DeviceExt.java
+++ dc3-common/dc3-common-model/src/main/java/io/github/pnoker/common/entity/ext/DeviceExt.java
@@ -26,10 +26,7 @@ import java.io.Serial;
 import java.io.Serializable;
 
 /**
- * Ext
- * <p>
- *
- * @author pnoker
+ * JSON extension object for device metadata and configuration.
  * @version 2025.9.0
  * @since 2016.10.1
  */
